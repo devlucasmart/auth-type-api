65774259b1f939d57c2b171a441d79139ec6049c
Lucas Martins <lucas.martins.fsa@gmail.com>
2024-07-09 17:43:12 -0300

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { UsersService } from './users/users.service';
+import { CreateUserDto } from './users/dto/create-user.dto';
+import { UpdateUserDto } from './users/dto/update-user.dto';
+
+@Controller('users')
+export class AppController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+}
